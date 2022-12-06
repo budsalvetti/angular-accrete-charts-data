133c1e74d057ed044b1e47cb42d547e2160fea86
budsalvetti <budsalvetti@gmail.com>
2022-12-06 15:03:12 -0800

    x-axis is now on top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,21 +31,26 @@ export class AppComponent {
       tickThickness: 0,
       labelFontSize: 0,
       tickLength: 0,
-      margin: -80
+      margin: -88
     },
-    axisX:{},
     data: [
       {
         indexLabel: "{y}",
         indexLabelPlacement: "outside",  
         indexLabelOrientation: "horizontal",
+        axisXType: "secondary",
         type: 'column',
         dataPoints: [
-          { label: 'apple', color:'#0085ff', y: 1000000000 },
-          { label: 'orange', color:'#0085ff', y: 2000000000 },
-          { label: 'banana', color:'#0085ff', y: 3000000000 },
-          { label: 'mango', color: '#0085ff', y: 4000000000 },
-          { label: 'grape', color: '#0085ff', y: 5000000000 },
+          { label: '2014', color:'#0085ff', y: 1000000000 },
+          { label: '2015', color:'#0085ff', y: 2000000000 },
+          { label: '2016', color:'#0085ff', y: 3000000000 },
+          { label: '2017', color: '#0085ff', y: 4000000000 },
+          { label: '2018', color: '#0085ff', y: 5000000000 },
+          { label: '2019', color: '#0085ff', y: 6000000000 },
+          { label: '2020', color: '#0085ff', y: 7000000000 },
+          { label: '2021', color: '#0085ff', y: 8000000000 },
+          { label: '2022', color: '#0085ff', y: 9000000000 },
+          { label: '2022', color: '#0085ff', y: 10000000000 },
         ],
       },
     ],
