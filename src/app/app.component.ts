438bae7c882315b18da958c4cbdbaee5dda58c02
budsalvetti <budsalvetti@gmail.com>
2022-12-06 14:50:21 -0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,23 +23,29 @@ export class AppComponent {
     animationEnabled: true,
     theme: 'dark1',
     title: {
-      text: 'Angular Column Chart in Material UI Tabs',
+      //text: 'Angular Column Chart in Material UI Tabs',
     },
     axisY:{
       gridThickness: 0,
       lineThickness: 0,
       tickThickness: 0,
-      labelFontSize: 0
+      labelFontSize: 0,
+      tickLength: 0,
+      margin: -80
     },
+    axisX:{},
     data: [
       {
+        indexLabel: "{y}",
+        indexLabelPlacement: "outside",  
+        indexLabelOrientation: "horizontal",
         type: 'column',
         dataPoints: [
-          { label: 'apple', color:'#0085ff', y: 10 },
-          { label: 'orange', color:'#0085ff', y: 15 },
-          { label: 'banana', color:'#0085ff', y: 25 },
-          { label: 'mango', color: '#0085ff', y: 30 },
-          { label: 'grape', color: '#0085ff', y: 28 },
+          { label: 'apple', color:'#0085ff', y: 1000000000 },
+          { label: 'orange', color:'#0085ff', y: 2000000000 },
+          { label: 'banana', color:'#0085ff', y: 3000000000 },
+          { label: 'mango', color: '#0085ff', y: 4000000000 },
+          { label: 'grape', color: '#0085ff', y: 5000000000 },
         ],
       },
     ],
