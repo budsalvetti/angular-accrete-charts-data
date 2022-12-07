081a4a0a616686dbdf7bcfb1385a491a92a6b295
budsalvetti <budsalvetti@gmail.com>
2022-12-06 23:36:37 -0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,59 +23,58 @@ export class AppComponent implements OnInit {
 
   constructor(private investmentDataService: InvestmentDataService) {}
 
-
   /**
    * @description Get the initial default data which is by company and set up form controls
    */
-     ngOnInit() {
+  ngOnInit() {
+    // get the data for the investment category dropdown
+    this.investmentCategories = this.investmentDataService.investmentCategories;
 
-      // get the data for the investment category dropdown
-      this.investmentCategories = this.investmentDataService.investmentCategories;
-  
-      this.investmentDataService
-        .getDefaultCompanyData()
-        .subscribe((chartData: any[]) => {
-          for (let i = 0; i < chartData.length; i++) {
-            const company = chartData[i];
-  
-            //shallow copy the default chart options
-            let newChartOptions = { ...this.investmentDataService.defaultChartOptions };
-  
-            if (i > 0) {
-              newChartOptions.axisX2 = {
-                lineThickness: 0,
-                tickThickness: 0,
-                labelFontSize: 0,
-              };
-            }
-  
-            newChartOptions['companyName'] = company.name;
-  
-            newChartOptions.data[0].dataPoints = [];
-  
-            for (let year of company['years']) {
-              newChartOptions.data[0].dataPoints.push({
-                label: year.year as string,
-                color: '#0085ff',
-                y: year['total'],
-              });
-            }
-  
-            this.chartData.push(newChartOptions);
+    this.investmentDataService
+      .getDefaultCompanyData()
+      .subscribe((chartData: any[]) => {
+        for (let i = 0; i < chartData.length; i++) {
+          const company = chartData[i];
+
+          //shallow copy the default chart options
+          let newChartOptions = {
+            ...this.investmentDataService.defaultChartOptions,
+          };
+
+          if (i > 0) {
+            newChartOptions.axisX2 = {
+              lineThickness: 0,
+              tickThickness: 0,
+              labelFontSize: 0,
+            };
           }
-        });
-    }
 
-    // dropdown change handler for main entity COMPANY or INDUSTRY 
-    public handleEntityChange(evt){
-      alert('here');
-    }
+          newChartOptions['companyName'] = company.name;
 
-    // dropdown change handler for investment category filter dropdown
-    public handleCatFilterChange(evt){
-      alert('here');
-    }
+          newChartOptions.data[0].dataPoints = [];
 
+          for (let year of company['years']) {
+            newChartOptions.data[0].dataPoints.push({
+              label: year.year as string,
+              color: '#0085ff',
+              y: year['total'],
+            });
+          }
+
+          this.chartData.push(newChartOptions);
+        }
+      });
+  }
+
+  // dropdown change handler for main entity COMPANY or INDUSTRY
+  public handleEntityChange(evt) {
+    alert('what is the evt ' + JSON.stringify(evt));
+  }
+
+  // dropdown change handler for investment category filter dropdown
+  public handleCatFilterChange(evt) {
+    alert('what is the evt ' + JSON.stringify(evt));
+  }
 
   public getChartOptions(i: number) {
     return this.chartData[i];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -12,7 +13,7 @@ import { InvestmentDataService } from './services/investment-data.service';
 
 @NgModule({
   declarations: [AppComponent, CanvasJSChart],
-  imports: [BrowserAnimationsModule, BrowserModule, CommonModule, MatTabsModule],
+  imports: [BrowserAnimationsModule, BrowserModule, CommonModule, MatTabsModule, FormsModule],
   providers: [InvestmentDataService],
   bootstrap: [AppComponent],
 })
